213eb4519e28616e8f35f09c86e7cc429de2a7a3
AbramFernandoH <abramfernando18@gmail.com>
2022-02-22 09:31:24 +0700

    adding header components and link to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -2,35 +2,16 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Routes, Route, useParams, useNavigate, Link } from 'react-router-dom';
-
-const HatsPage = ({barang}) => (
-  <div className='hats'>
-    <h1>Halaman {barang}</h1>
-  </div>
-);
-
-const HatsDetailPage = (props) => {
-  const params = useParams();
-  const navigate = useNavigate();
-  return (
-    <div className='hats'>
-      <h1>Halaman Topi ke {params.hatId}</h1>
-
-      <button onClick={() => navigate('/')}>ke homepage</button>
-      <button onClick={() => navigate(-2)}>balik 2 halaman ke belakang</button>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/hats" element={<HatsPage barang="Baju" />} />
         <Route path="shop" element={<ShopPage />} />
-        <Route path="shop/hats" element={<HatsDetailPage />} />
       </Routes>
     </div>
   );
